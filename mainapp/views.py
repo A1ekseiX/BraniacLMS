028388157ea03d82ba6e81c1b83bd124b29d7263
A1ekseiX <al.m.az4409@gmail.com>
2023-02-05 00:05:20 +0300

    Lesson_2 Static site with Django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,28 @@
-from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 
-class HelloWorldView(View):
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/corses_list.html"
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+
+
+
+class HelloWorldView(TemplateView):
     def get(self, *args):
         return HttpResponse("Hellow world!")
 
