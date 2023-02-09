0948e2e5c9fb102b904bc68a4caaa1b418aaa547
A1ekseiX <91829886+A1ekseiX@users.noreply.github.com>
2023-02-09 23:33:16 +0300

    Lesson 2 (#1)
    * Preconfig
    
    * Delete JetBrains.gitignore
    
    * lesson_2 Hello world with CBV and FBV
    
    * Lesson_2 Static site with Django
    
    * Lesson_2 Code style fix

---
+++ mainapp/views.py
@@ -0,0 +1,34 @@
+from django.views.generic import TemplateView
+
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/corses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+
+
+class HelloWorldView(TemplateView):
+    def get(self, *args):
+        return HttpResponse("Hellow world!")
+
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse(f"kwargs:<br>{kwargs}")
